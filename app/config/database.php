5842366fe6ce9c2e245c6d3e90e6d050b9735a0c
ZeanGeeks <zikukato48@gmail.com>
2026-09-09 21:51:59 +0800

    Complete Aiven CRUD authentication

--- app/config/database.php
+++ app/config/database.php
@@ -58,11 +58,11 @@ defined('PREVENT_DIRECT_ACCESS') OR exit('No direct script access allowed');
 */
 $database['main'] = array(
     'driver'	=> getenv('DB_DRIVER') ?: 'mysql',
-    'hostname'	=> getenv('DB_HOST') ?: 'localhost',
-    'port'		=> getenv('DB_PORT') ?: '3306',
-    'username'	=> getenv('DB_USER') ?: 'root',
+    'hostname'	=> getenv('DB_HOST') ?: 'mysql-26409035-mantalazean-act5.b.aivencloud.com',
+    'port'		=> getenv('DB_PORT') ?: '19646',
+    'username'	=> getenv('DB_USER') ?: 'avnadmin',
     'password'	=> getenv('DB_PASSWORD') ?: '',
-    'database'	=> getenv('DB_NAME') ?: 'lavalust_crud',
+    'database'	=> getenv('DB_NAME') ?: 'defaultdb',
     'charset'	=> getenv('DB_CHARSET') ?: 'utf8mb4',
     'dbprefix'	=> getenv('DB_PREFIX') ?: '',
     // Optional for SQLite
